5500ab1c077e112fb177328ca173c57b45518a91
mirandarevans <miranda.r.evans@gmail.com>
2018-08-27 12:47:57 -0700

    pep8 on 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -16,9 +16,8 @@ if __name__ == "__main__":
     tasks = r.json()
     all_tasks = [item for item in tasks if item.get('userId') == emp_id]
 
-
     with open('2.csv', 'w+') as f:
         for item in all_tasks:
             f.write('"{}","{}","{}","{}"\n'.format(emp_id, emp_name,
-                                                 item.get('completed'),
-                                                 item.get('title')))
+                                                   item.get('completed'),
+                                                   item.get('title')))
